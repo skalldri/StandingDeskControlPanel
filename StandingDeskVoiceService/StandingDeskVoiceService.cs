af497682ee18f61b6171728fa5042e88ac09fbeb
Stuart Alldritt <s.k.alldritt@gmail.com>
2017-02-07 00:43:30 -0800

    Adding more meat to the background service. Still pulled straight from the UWP sample.

--- StandingDeskVoiceService/StandingDeskVoiceService.cs
+++ StandingDeskVoiceService/StandingDeskVoiceService.cs
@@ -10,11 +10,103 @@ namespace StandingDeskControlPanel.VoiceService
 {
     public sealed class StandingDeskVoiceService : IBackgroundTask
     {
+        /// <summary>
+        /// the service connection is maintained for the lifetime of a cortana session, once a voice command
+        /// has been triggered via Cortana.
+        /// </summary>
+        VoiceCommandServiceConnection voiceServiceConnection;
+
+        /// <summary>
+        /// Lifetime of the background service is controlled via the BackgroundTaskDeferral object, including
+        /// registering for cancellation events, signalling end of execution, etc. Cortana may terminate the 
+        /// background service task if it loses focus, or the background task takes too long to provide.
+        /// 
+        /// Background tasks can run for a maximum of 30 seconds.
+        /// </summary>
+        BackgroundTaskDeferral serviceDeferral;
+
+        /// <summary>
+        /// ResourceMap containing localized strings for display in Cortana.
+        /// </summary>
+        ResourceMap cortanaResourceMap;
+
+        /// <summary>
+        /// The context for localized strings.
+        /// </summary>
+        ResourceContext cortanaContext;
+
+        /// <summary>
+        /// Get globalization-aware date formats.
+        /// </summary>
+        DateTimeFormatInfo dateFormatInfo;
+
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // TODO: add background service code.
-            // Background services should actually do most of the work for this application
+            serviceDeferral = taskInstance.GetDeferral();
 
+            // Register to receive an event if Cortana dismisses the background task. This will
+            // occur if the task takes too long to respond, or if Cortana's UI is dismissed.
+            // Any pending operations should be cancelled or waited on to clean up where possible.
+            taskInstance.Canceled += OnTaskCanceled;
+
+            var triggerDetails = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+
+            // Load localized resources for strings sent to Cortana to be displayed to the user.
+            cortanaResourceMap = ResourceManager.Current.MainResourceMap.GetSubtree("Resources");
+
+            // Select the system language, which is what Cortana should be running as.
+            cortanaContext = ResourceContext.GetForViewIndependentUse();
+
+            // Get the currently used system date format
+            dateFormatInfo = CultureInfo.CurrentCulture.DateTimeFormat;
+
+            // This should match the uap:AppService and VoiceCommandService references from the 
+            // package manifest and VCD files, respectively. Make sure we've been launched by
+            // a Cortana Voice Command.
+            if (triggerDetails != null && triggerDetails.Name == "AdventureWorksVoiceCommandService")
+            {
+                try
+                {
+                    voiceServiceConnection =
+                        VoiceCommandServiceConnection.FromAppServiceTriggerDetails(
+                            triggerDetails);
+
+                    voiceServiceConnection.VoiceCommandCompleted += OnVoiceCommandCompleted;
+
+                    // GetVoiceCommandAsync establishes initial connection to Cortana, and must be called prior to any 
+                    // messages sent to Cortana. Attempting to use ReportSuccessAsync, ReportProgressAsync, etc
+                    // prior to calling this will produce undefined behavior.
+                    VoiceCommand voiceCommand = await voiceServiceConnection.GetVoiceCommandAsync();
+
+                    // Depending on the operation (defined in AdventureWorks:AdventureWorksCommands.xml)
+                    // perform the appropriate command.
+                    switch (voiceCommand.CommandName)
+                    {
+                        case "whenIsTripToDestination":
+                            var destination = voiceCommand.Properties["destination"][0];
+                            await SendCompletionMessageForDestination(destination);
+                            break;
+                        case "cancelTripToDestination":
+                            var cancelDestination = voiceCommand.Properties["destination"][0];
+                            await SendCompletionMessageForCancellation(cancelDestination);
+                            break;
+                        default:
+                            // As with app activation VCDs, we need to handle the possibility that
+                            // an app update may remove a voice command that is still registered.
+                            // This can happen if the user hasn't run an app since an update.
+                            LaunchAppInForeground();
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("Handling Voice Command failed " + ex.ToString());
+                }
+            }
+        }
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
             throw new NotImplementedException();
         }
     }
