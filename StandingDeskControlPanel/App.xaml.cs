482c7bf183b8f0f041b998982fb340937b7d3dfe
Stuart Alldritt <s.k.alldritt@gmail.com>
2017-02-07 00:35:33 -0800

    Adding skeleton code based on the UWP Cortana sample

--- StandingDeskControlPanel/App.xaml.cs
+++ StandingDeskControlPanel/App.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.Media.SpeechRecognition;
 
 namespace StandingDeskControlPanel
 {
@@ -37,7 +39,7 @@ namespace StandingDeskControlPanel
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        protected async override void OnLaunched(LaunchActivatedEventArgs e)
         {
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
@@ -77,6 +79,35 @@ namespace StandingDeskControlPanel
                 // Ensure the current window is active
                 Window.Current.Activate();
             }
+
+            try
+            {
+                // Install the main VCD. 
+                StorageFile vcdStorageFile = await Package.Current.InstalledLocation.GetFileAsync(@"StandingDeskControlPanelVoiceCommands.xml");
+
+                await Windows.ApplicationModel.VoiceCommands.VoiceCommandDefinitionManager.InstallCommandDefinitionsFromStorageFileAsync(vcdStorageFile);
+                
+                // This is where we update the voice command definition so that we include custom targets. The voice commands contain 
+                // {height} slots, and there is a fixed number of words which work in that slot.
+                // We call this method to update the pre-defined list of words which work in that slot so that anything the user defines
+                // will properly trigger Cortana.
+
+                /*
+                 * 
+                // Update phrase list.
+                ViewModel.ViewModelLocator locator = App.Current.Resources["ViewModelLocator"] as ViewModel.ViewModelLocator;
+                if (locator != null)
+                {
+                    await locator.TripViewModel.UpdateDestinationPhraseList();
+                }
+
+                */
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Installing Voice Commands Failed: " + ex.ToString());
+            }
+
         }
 
         /// <summary>
@@ -101,6 +132,134 @@ namespace StandingDeskControlPanel
             var deferral = e.SuspendingOperation.GetDeferral();
             //TODO: Save application state and stop any background activity
             deferral.Complete();
+        }
+
+        /// <summary>
+        /// OnActivated is the entry point for an application when it is launched via
+        /// means other normal user interaction. This includes Voice Commands, URI activation,
+        /// being used as a share target from another app, etc. Here, we're going to handle the
+        /// Voice Command activation from Cortana.
+        /// 
+        /// Note: Be aware that an older VCD could still be in place for your application if you
+        /// modify it and update your app via the store. You should be aware that you could get 
+        /// activations that include commands in older versions of your VCD, and you should try
+        /// to handle them gracefully.
+        /// </summary>
+        /// <param name="args">Details about the activation method, including the activation
+        /// phrase (for voice commands) and the semantic interpretation, parameters, etc.</param>
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            base.OnActivated(args);
+
+            Type navigationToPageType;
+            ViewModel.TripVoiceCommand? navigationCommand = null;
+
+            // If the app was launched via a Voice Command, this corresponds to the "show trip to <location>" command. 
+            // Protocol activation occurs when a tile is clicked within Cortana (via the background task)
+            if (args.Kind == ActivationKind.VoiceCommand)
+            {
+                // The arguments can represent many different activation types. Cast it so we can get the
+                // parameters we care about out.
+                var commandArgs = args as VoiceCommandActivatedEventArgs;
+
+                Windows.Media.SpeechRecognition.SpeechRecognitionResult speechRecognitionResult = commandArgs.Result;
+
+                // Get the name of the voice command and the text spoken. See AdventureWorksCommands.xml for
+                // the <Command> tags this can be filled with.
+                string voiceCommandName = speechRecognitionResult.RulePath[0];
+                string textSpoken = speechRecognitionResult.Text;
+
+                // The commandMode is either "voice" or "text", and it indictes how the voice command
+                // was entered by the user.
+                // Apps should respect "text" mode by providing feedback in silent form.
+                string commandMode = this.SemanticInterpretation("commandMode", speechRecognitionResult);
+
+                switch (voiceCommandName)
+                {
+                    case "showTripToDestination":
+                        // Access the value of the {destination} phrase in the voice command
+                        string destination = this.SemanticInterpretation("destination", speechRecognitionResult);
+
+                        // Create a navigation command object to pass to the page. Any object can be passed in,
+                        // here we're using a simple struct.
+                        navigationCommand = new ViewModel.TripVoiceCommand(
+                            voiceCommandName,
+                            commandMode,
+                            textSpoken,
+                            destination);
+
+                        // Set the page to navigate to for this voice command.
+                        navigationToPageType = typeof(View.TripDetails);
+                        break;
+                    default:
+                        // If we can't determine what page to launch, go to the default entry point.
+                        navigationToPageType = typeof(View.TripListView);
+                        break;
+                }
+            }
+            else if (args.Kind == ActivationKind.Protocol)
+            {
+                // Extract the launch context. In this case, we're just using the destination from the phrase set (passed
+                // along in the background task inside Cortana), which makes no attempt to be unique. A unique id or 
+                // identifier is ideal for more complex scenarios. We let the destination page check if the 
+                // destination trip still exists, and navigate back to the trip list if it doesn't.
+                var commandArgs = args as ProtocolActivatedEventArgs;
+                Windows.Foundation.WwwFormUrlDecoder decoder = new Windows.Foundation.WwwFormUrlDecoder(commandArgs.Uri.Query);
+                var destination = decoder.GetFirstValueByName("LaunchContext");
+
+                navigationCommand = new ViewModel.TripVoiceCommand(
+                                        "protocolLaunch",
+                                        "text",
+                                        "destination",
+                                        destination);
+
+                navigationToPageType = typeof(View.TripDetails);
+            }
+            else
+            {
+                // If we were launched via any other mechanism, fall back to the main page view.
+                // Otherwise, we'll hang at a splash screen.
+                navigationToPageType = typeof(View.TripListView);
+            }
+
+            // Re"peat the same basic initialization as OnLaunched() above, taking into account whether
+            // or not the app is already active.
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+                App.NavigationService = new NavigationService(rootFrame);
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            // Since we're expecting to always show a details page, navigate even if 
+            // a content frame is in place (unlike OnLaunched).
+            // Navigate to either the main trip list page, or if a valid voice command
+            // was provided, to the details page for that trip.
+            rootFrame.Navigate(navigationToPageType, navigationCommand);
+
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Returns the semantic interpretation of a speech result. Returns null if there is no interpretation for
+        /// that key.
+        /// </summary>
+        /// <param name="interpretationKey">The interpretation key.</param>
+        /// <param name="speechRecognitionResult">The result to get an interpretation from.</param>
+        /// <returns></returns>
+        private string SemanticInterpretation(string interpretationKey, SpeechRecognitionResult speechRecognitionResult)
+        {
+            return speechRecognitionResult.SemanticInterpretation.Properties[interpretationKey].FirstOrDefault();
         }
     }
 }
---
+++ StandingDeskVoiceService/StandingDeskVoiceService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+
+namespace StandingDeskControlPanel.VoiceService
+{
+    public sealed class StandingDeskVoiceService : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // TODO: add background service code.
+            // Background services should actually do most of the work for this application
+
+            throw new NotImplementedException();
+        }
+    }
+}
